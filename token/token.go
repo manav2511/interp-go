883706aa3fdf8664e1d2c3e716e6d9b2c8c88c48
Manav Raghuwanshi <manav2511@gmail.com>
2025-03-14 18:12:42 +0530

    feat: add basic tokens to lexer

---
+++ token/token.go
@@ -0,0 +1,37 @@
+// token/token.go
+
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT" // 1324561
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
